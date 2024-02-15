eed843c8e18bb9263f09b1848e5ccbc430609218
Alexandra <atoxtaeva@gmail.com>
2024-02-15 14:53:18 +0300

    add some variables lesson1 task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+
+#pragma warning disable CS0219 // Переменная назначена, но ее значение не используется
+int value = 100;
+#pragma warning restore CS0219 // Переменная назначена, но ее значение не используется
+Console.WriteLine("value");
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
+int val = 100;
+Console.WriteLine(val);
